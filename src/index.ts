ea0f039f6861e6a9af29ac7ce57e9348b70497f2
Youhei Oonuma <oonuma617@gmail.com>
2020-09-15 22:20:09 +0900

    iroiro

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello");
+import * as https from 'https'
+
+const config = require('config');
+
+const api = config.get("Youtube.apikey");
+
+const url = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + api + "&part=snippet,contentDetails,statistics,status";
+
+
+const request = https.request(url, res => {
+
+    let data = "";
+
+    res.on("data", chunk => {
+        data += chunk;
+    })
+
+    res.on("end", () => {
+        let resData = JSON.parse(data)
+
+        console.log(resData.kind)
+    })
+})
+
+request.end()
