0c068cf783fe135e9618605a8512316c901c1626
Youhei Oonuma <oonuma617@gmail.com>
2020-09-16 06:38:52 +0900

    iroiro

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-import * as https from 'https'
+import * as https from 'https';
+import * as querystring from 'querystring';
 
 const config = require('config');
 
+// fetch config/default.json
 const api = config.get("Youtube.apikey");
 
-const url = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + api + "&part=snippet,contentDetails,statistics,status";
+const activity_url = "https://www.googleapis.com/youtube/v3/activities";
 
+const query = {
+    key: api,
+    channelId: "UCFKOVgVbGmX65RxO3EtH3iw",
+    part: "id,snippet,contentDetails"
+}
 
-const request = https.request(url, res => {
+const url = activity_url + "?" + querystring.encode(query);
+
+console.log(url);
+
+const request = https.get(url, res => {
 
     let data = "";
 
@@ -18,8 +29,8 @@ const request = https.request(url, res => {
     res.on("end", () => {
         let resData = JSON.parse(data)
 
-        console.log(resData.kind)
+        console.log(resData.items)
     })
 })
+// request.end()
 
-request.end()
