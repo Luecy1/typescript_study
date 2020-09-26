762b30f1b5c7d445846e6079414c9c2548e6fe94
Youhei Oonuma <oonuma617@gmail.com>
2020-09-26 20:35:46 +0900

    update http

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,22 +1,6 @@
-import * as https from 'https';
+import fetch from 'node-fetch';
 
-export function get(url: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-        const request = https.get(url, res => {
-            let data = "";
-
-            res.on("data", chunk => {
-                data += chunk;
-            });
-
-            res.on("end", () => {
-                resolve(data);
-            });
-
-            res.on("error", () => {
-                reject();
-            });
-        });
-    });
+export async function get(url: string): Promise<string> {
+    const response = await fetch(url);
+    return await response.text()
 }
-
